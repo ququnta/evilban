ab69b57aff0042ee124491a0a99282ad2e2f68bc
Alex <alex@skytech.lt>
2025-06-10 17:55:37 +0300

    config file in install missing

--- install_evilban.sh
+++ install_evilban.sh
@@ -30,7 +30,7 @@ sudo mkdir -p "$LOGDIR"
 sudo chmod 755 "$LOGDIR"
 
 echo "[*] Copying files..."
-sudo cp evilban_provider_by_range.sh evilban_deflate_scan.sh evilban_cidr_aggregate.sh evilban_providers.txt README.txt "$INSTALL_DIR/"
+sudo cp evilban.conf evilban_provider_by_range.sh evilban_deflate_scan.sh evilban_cidr_aggregate.sh evilban_providers.txt README.txt "$INSTALL_DIR/"
 sudo cp "$CONF_SRC" "$CONF_DST"
 sudo chmod +x "$INSTALL_DIR/"*.sh
 
